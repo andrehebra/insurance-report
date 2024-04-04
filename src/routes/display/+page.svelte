a19a50a110169f01e43dfb8604f6b41f1829f96e
andrehebra <52555960+andrehebra@users.noreply.github.com>
2024-04-04 13:07:53 -0400

    working, but not correcting for flights logged under incorrect registration when compared to the original, will have to look into it

--- src/routes/display/+page.svelte
+++ src/routes/display/+page.svelte
@@ -104,11 +104,13 @@
         let beginPeriod = beginDate.toISOString().slice(0, -5);
         let endPeriod = endDate.toISOString().slice(0, -5);
 
+        console.log(dataArray);
+
         for (let i = 0; i < dataArray.length; i++) {
             if (dataArray[i].aircraft != null) {
                 if (dataArray[i].registration != null) {
                     if (dataArray[i].registration.flights[0] != null && dataArray[i].registration.flights[0].primaryLog.finishSeconds != null && dataArray[i].registration.flights[0].primaryLog.startSeconds != null) {
-                        //console.log(dataArray[i]);
+                        
                         //check to see if callsign is included in the aircraft list already
                         let caught = false;
                         for (let j = 0; j < aircraftList.length; j++) {
@@ -158,126 +160,6 @@
         aircraftList = aircraftList;
         
     }
-
-    function calculate2() {
-
-        instructorList.splice(0,instructorList.length);
-
-        let currentSelectedPeriod = new Date(selectedPeriod);
-        let endOfPeriod = new Date(currentSelectedPeriod.getTime() + (14*24*60*60*1000));
-        
-
-        for (let i = 0; i < dataArray.length; i++) {
-            //console.log(dataArray[i]);
-            let caught = 0;
-            if (dataArray[i] != null && dataArray[i].instructor != null && dataArray[i].registration != null && dataArray[i].startsAt != null) {
-                if (dataArray[i].instructor.firstName != "Dispatch" && dataArray[i].startsAt >= currentSelectedPeriod.toISOString().slice(0, -5) && dataArray[i].startsAt < endOfPeriod.toISOString().slice(0, -5)) {
-                    for (let j = 0; j < instructorList.length; j++) {
-                        if (dataArray[i].instructor.firstName == instructorList[j].firstName && dataArray[i].instructor.lastName == instructorList[j].lastName) {
-                            caught = 1;
-                        }
-                    }
-
-                    if (caught == 0) {
-                        instructorList.push({
-                            firstName: dataArray[i].instructor.firstName,
-                            lastName: dataArray[i].instructor.lastName,
-                            briefingSeconds: dataArray[i].registration.briefingSeconds,
-                            totalSeconds: dataArray[i].registration.totalSeconds,
-                            debriefingSeconds: dataArray[i].registration.debriefingSeconds,
-                            reservationList: [dataArray[i]]
-                        })
-                    } else if (caught == 1) {
-                        for (let j = 0; j < instructorList.length; j++) {
-                            if (dataArray[i].instructor.firstName == instructorList[j].firstName && dataArray[i].instructor.lastName == instructorList[j].lastName) {
-                                instructorList[j].briefingSeconds += dataArray[i].registration.briefingSeconds;
-                                instructorList[j].totalSeconds += dataArray[i].registration.totalSeconds;
-                                instructorList[j].debriefingSeconds += dataArray[i].registration.debriefingSeconds;
-                                instructorList[j].reservationList.push(dataArray[i]);
-                            }
-                        }
-                    } else {
-                        console.log("This was not caught for some reason" + dataArray[i]);
-                    }
-                }
-                
-            } else if (dataArray[i] != null && dataArray[i].instructor != null && dataArray[i].registrations != null && dataArray[i].startsAt != null) {
-                        if (dataArray[i].instructor.firstName != "Dispatch" && dataArray[i].startsAt >= currentSelectedPeriod.toISOString().slice(0, -5) && dataArray[i].startsAt < endOfPeriod.toISOString().slice(0, -5)) {
-                            for (let j = 0; j < instructorList.length; j++) {
-                                if (dataArray[i].instructor.firstName == instructorList[j].firstName && dataArray[i].instructor.lastName == instructorList[j].lastName) {
-                                    caught = 1;
-                                }
-                            }
-
-                            //console.log(dataArray[i]);
-
-                            let max = {
-                                brief: 0,
-                                total: 0,
-                                debrief: 0
-                            }
-
-                            for (let j = 0; j < dataArray[i].registrations.length; j++) {
-                                if (dataArray[i].registrations[j].briefingSeconds > max.brief) {
-                                    max.brief = dataArray[i].registrations[j].briefingSeconds;
-                                }
-                                if (dataArray[i].registrations[j].totalSeconds > max.total) {
-                                    max.total = dataArray[i].registrations[j].totalSeconds;
-                                }
-                                if (dataArray[i].registrations[j].debriefingSeconds > max.debrief) {
-                                    max.debrief = dataArray[i].registrations[j].debriefingSeconds;
-                                }
-                            }
-
-                            if (caught == 0) {
-                                instructorList.push({
-                                    firstName: dataArray[i].instructor.firstName,
-                                    lastName: dataArray[i].instructor.lastName,
-                                    briefingSeconds: max.brief,
-                                    totalSeconds: max.total,
-                                    debriefingSeconds: max.debrief,
-                                    reservationList: [dataArray[i]]
-                                })
-                            } else if (caught == 1) {
-                                for (let j = 0; j < instructorList.length; j++) {
-                                    if (dataArray[i].instructor.firstName == instructorList[j].firstName && dataArray[i].instructor.lastName == instructorList[j].lastName) {
-                                        instructorList[j].briefingSeconds += max.brief;
-                                        instructorList[j].totalSeconds += max.total;
-                                        instructorList[j].debriefingSeconds += max.debrief;
-                                        instructorList[j].reservationList.push(dataArray[i]);
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                //console.log(dataArray[i]);
-            }
-            
-        }
-
-        if (instructorList.length == 0) {
-            instructorList.push({
-                firstName: "No Entries Found",
-                lastName: "",
-                briefingSeconds: "",
-                totalSeconds: "",
-                debriefingSeconds: "",
-                reservationList: []
-            })
-        }
-
-        instructorList = instructorList;
-
-        instructorList.sort(compareLast);
-        instructorList.sort(compareFirst);
-
-        filled = true;
-
-        
-        console.log(instructorList);
-        //console.log(instructorList);
-        //console.log(selectedPeriod);
-    }
     
 </script>
 
@@ -308,7 +190,7 @@
 
 
 
-<Heading tag='h2'>Instructor Hours for Period</Heading>
+<Heading tag='h2'>Hobbs Hours Per Aircraft</Heading>
 <div class="padding"></div>
 <Table hoverable={true} shadow>
     <TableHead>
@@ -318,6 +200,8 @@
         <TableHeadCell>Debfiefing</TableHeadCell> -->
         <TableHeadCell>Hobbs Start</TableHeadCell>
         <TableHeadCell>Hobbs End</TableHeadCell>
+        <TableHeadCell>Total Hobbs Time</TableHeadCell>
+        
     </TableHead>
     <TableBody>
         {#each aircraftList as aircraft}
@@ -328,6 +212,7 @@
                 <TableBodyCell>{instructor.debriefingSeconds / 60 / 60}</TableBodyCell> -->
                 <TableBodyCell>{aircraft.beginningSeconds / 60 / 60}</TableBodyCell>
                 <TableBodyCell>{aircraft.endingSeconds / 60 / 60}</TableBodyCell>
+                <TableBodyCell>{Math.round(((aircraft.endingSeconds / 60 / 60)-(aircraft.beginningSeconds / 60 / 60))*10) / 10}</TableBodyCell>
             </TableBodyRow>
         {/each}
     </TableBody>
