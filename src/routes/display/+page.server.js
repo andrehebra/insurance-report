7f1723ce649d52b9335d2f4303a5fe2d288d917a
andrehebra <andrehebra@gmail.com>
2024-04-03 20:57:44 -0400

    copied files from hours tracker

---
+++ src/routes/display/+page.server.js
@@ -0,0 +1,117 @@
+import { API_KEY } from '$env/static/private';
+
+import gql from 'graphql-tag';
+import { print } from 'graphql';
+
+import { error } from '@sveltejs/kit';
+
+const query = `query Bookings($all: Boolean, $after: String, $to: DateTime) {
+    bookings(all: $all, after: $after, to: $to) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      nodes {
+        ... on SingleStudentBooking {
+          startsAt
+          endsAt
+          instructor {
+            firstName
+            lastName
+          }
+          registration {
+            briefingSeconds
+            totalSeconds
+            debriefingSeconds
+          }
+        }
+        ... on MultiStudentBooking {
+          startsAt
+          endsAt
+          instructor {
+            firstName
+            lastName
+          }
+          registrations {
+            briefingSeconds
+            totalSeconds
+            debriefingSeconds
+          }
+        }
+      }
+    }
+  }`;
+
+let currentDate = new Date();
+
+//console.log(currentDate);
+//const pastDate = new Date(); 
+//pastDate = pastDate.setDate(pastDate.getDate() - 45);
+//console.log(pastDate);
+
+const currentISODate = currentDate.toISOString().slice(0, -5);
+
+export const load = async () => {
+    console.log('Load function called in page.server.js');
+    try {
+        const variables = {
+            to: currentISODate,
+            all: true,
+        };
+
+        const response = await fetch('https://api.flightlogger.net/graphql', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query,
+                variables
+            })
+        });
+
+        let data = await response.json();
+        let dataArray = [...data.data.bookings.nodes];
+        //console.log(dataArray);
+
+        while (data.data.bookings.pageInfo.hasNextPage == true) {
+            const variables = {
+                all: true,
+                to: currentISODate,
+                after: data.data.bookings.pageInfo.endCursor,
+            }
+
+            const response = await fetch('https://api.flightlogger.net/graphql', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${API_KEY}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query,
+                    variables
+                })
+            });
+
+            console.log(data.data.bookings.pageInfo.hasNextPage);
+
+            data = await response.json();
+            try {
+                for (let i = 0; i < data.data.bookings.nodes.length; i++) {
+                    //console.log(data.data.bookings.nodes);
+                    dataArray.push(data.data.bookings.nodes[i]);
+                }
+            } catch (e) {
+                console.error("concatination error: " + e);
+
+                return {  };
+            }
+
+        }
+
+        return { dataArray };
+    } catch (error) {
+        console.error(`Error in load function :( ${error}`);
+    }
+}
